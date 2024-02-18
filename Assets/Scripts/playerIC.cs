1fd19e397f8fab94ac2b33e7f69e3888994aff4f
alandaz <alandazong@gmail.com>
2024-02-18 15:07:37 -0600

    adding goal

--- Assets/Scripts/playerIC.cs
+++ Assets/Scripts/playerIC.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerc : MonoBehaviour
+public class playerIC : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public GameObject playeric2; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +13,7 @@ public class playerc : MonoBehaviour
         
         rb.bodyType = RigidbodyType2D.Kinematic;
 
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(collision.gameObject); 
-            Destroy(gameObject); 
-            Instantiate(playeric2, collision.transform.position, Quaternion.identity);
         
-        }
     }
 
     // Update is called once per frame
