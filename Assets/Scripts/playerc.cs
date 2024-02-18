df2f3d68415157aa7031eaeb62897b8be3b7faa9
alandaz <alandazong@gmail.com>
2024-02-18 14:10:07 -0600

    saving

---
+++ Assets/Scripts/playerc.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
