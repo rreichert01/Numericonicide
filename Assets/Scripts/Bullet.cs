7387df466bb41383e513b793d92686817edb40aa
azbao <91306114+azbao@users.noreply.github.com>
2024-02-27 10:18:10 -0600

    revised bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,13 +6,19 @@ public class Bullet : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Platform")
-        {
-            Destroy(gameObject);
-        }
+        // if (collision.collider.tag == "Platform")
+        // {
+        //     Destroy(gameObject);
+        // }
+        // if (collision.collider.tag == "enemybullet")
+        // {
+        //     Destroy(gameObject); 
+        //     Destroy(collision.gameObject); 
+        // }
+
+        Destroy(gameObject);
         if (collision.collider.tag == "enemybullet")
         {
-            Destroy(gameObject); 
             Destroy(collision.gameObject); 
         }
 
